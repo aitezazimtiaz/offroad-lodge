b4d07d82437a53ad78097c474bf5fccb4883ec8d
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-02-27 16:23:32 +0500

    feat: ui colors initialize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,28 @@ export default {
       // fontFamily: {
       //   sans: ["var(--font-sans)", ...fontFamily.sans],
       // },
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        heading: {
+          DEFAULT: "hsl(var(--heading))",
+          foreground: "hsl(var(--heading-foreground))",
+        },
+        subheading: {
+          DEFAULT: "hsl(var(--subheading))",
+          foreground: "hsl(var(--subheading-foreground))",
+        },
+        titleheading: {
+          DEFAULT: "hsl(var(--titleheading))",
+          foreground: "hsl(var(--titleheading-foreground))",
+        },
+      },
     },
   },
   plugins: [],
