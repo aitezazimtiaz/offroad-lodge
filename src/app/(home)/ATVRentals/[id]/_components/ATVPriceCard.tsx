28a9b6d07e8ec44f7596a12d9253e1a9509ee001
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-03-01 15:06:01 +0500

    feat: atv-rental-details screen added

---
+++ src/app/(home)/ATVRentals/[id]/_components/ATVPriceCard.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+export default function ATVPriceCard() {
+  return (
+    <>
+      <div className="flex items-center gap-3 p-4 bg-secondary rounded-xl  justify-end max-sm:flex-wrap max-sm:justify-center">
+        {/* <div>
+          <span className="text-red-500 font-bold text-lg">$325</span>
+          <span>/per Day</span>
+        </div> */}
+        <Button className="font-bold text-white rounded-lg px-4 min-w-48">
+          Reserve
+        </Button>
+      </div>
+    </>
+  );
+}
