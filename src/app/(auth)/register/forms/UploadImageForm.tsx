e95e50b534c0fe08abfafc635c09c10507bfeec9
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-03-06 02:50:15 +0500

    fix typescript errors

--- src/app/(auth)/register/forms/UploadImageForm.tsx
+++ src/app/(auth)/register/forms/UploadImageForm.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 "use client";
 import React from "react";
 import { useFormik } from "formik";
