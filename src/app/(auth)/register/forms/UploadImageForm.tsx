561970f577e9ce723946076134bd43f11b254bcd
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-03-06 02:59:32 +0500

    fix linter issue FileList

--- src/app/(auth)/register/forms/UploadImageForm.tsx
+++ src/app/(auth)/register/forms/UploadImageForm.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 "use client";
 import React from "react";
@@ -25,10 +24,12 @@ const imageForm = z.object({
     .any()
     .optional()
     .refine(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       (files) => files?.[0]?.size <= MAX_FILE_SIZE,
       `Max image size is 5MB.`
     )
     .refine(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
       "Only .jpg, .jpeg, .png and .webp formats are supported."
     ),
@@ -46,7 +47,9 @@ const UploadImageForm = () => {
   });
 
   const imagePreview =
-    formik.values.image instanceof FileList && formik.values.image?.[0]
+    typeof window !== "undefined" &&
+    formik.values.image instanceof FileList &&
+    formik.values.image?.[0]
       ? URL.createObjectURL(formik.values.image?.[0])
       : null;
 
