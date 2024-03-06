1b0b97d687cdc453b4d688a32db03fc404faddbc
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-03-06 17:59:34 +0500

    fix ui routing

--- src/app/(inside_home)/_components/CommunityCard.tsx
+++ src/app/(inside_home)/_components/CommunityCard.tsx
@@ -8,20 +8,32 @@ import Image from "next/image";
 export function CardWithForm() {
   return (
     <div className="flex justify-center items-center max-md:w-full h-full w-3/4 ">
-      <Card className="w-full bg-secondary">
+      <Card className="w-full bg-secondary border-none p-4">
         <CardContent className="mt-2 mb-8">
           {CommunityData.map((data, index) => (
             <div key={data.id} className={index !== 0 ? "mt-4" : ""}>
               <div className="flex items-center space-x-4">
-                <Image src={data.avatar} width={50} height={50} className="rounded-full" alt="avatar"/>
+                <Image
+                  src={data.avatar}
+                  width={60}
+                  height={60}
+                  className="rounded-full"
+                  alt="avatar"
+                />
                 <div>
-                  <p className="text-base text-heading font-semibold">{data.userName}</p>
-                  <p className="text-xs text-subheading">{data.date} {data.time}</p>
+                  <p className="text-base text-heading font-semibold">
+                    {data.userName}
+                  </p>
+                  <p className="text-xs text-subheading">
+                    {data.date} {data.time}
+                  </p>
                 </div>
               </div>
               <div className="flex flex-col mt-4">
                 <div className="flex items-center justify-between">
-                  <p className="text-base text-primary font-semibold">{data.title}</p>
+                  <p className="text-base text-primary font-semibold">
+                    {data.title}
+                  </p>
                 </div>
                 <div className="mt-2">
                   <p className="text-sm text-subheading">{data.description}</p>
